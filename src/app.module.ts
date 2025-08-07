4217cddc5824a489c4d76e76300e1ad152162ac6
vladtymchuk <tymchukvlad177@gmail.com>
2025-08-07 16:57:02 +0300

    docker compose & typeorm install & db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,23 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MovieModule } from './movie/movie.module';
-import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { getTypeOrmConfig } from './config/typeorm.config';
 
 @Module({
   controllers: [AppController],
   providers: [AppService],
-  imports: [MovieModule, UserModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: getTypeOrmConfig,
+      inject: [ConfigService],
+    }),
+    MovieModule,
+  ],
 })
 export class AppModule {}
--- src/movie/movie.service.ts
+++ src/movie/movie.service.ts
@@ -1,11 +1,4 @@
 import { Injectable } from '@nestjs/common';
-import { UserService } from 'src/user/user.service';
 
 @Injectable()
-export class MovieService {
-  constructor(private readonly userService: UserService) {}
-
-  async test() {
-    return this.userService.test();
-  }
-}
+export class MovieService {}
