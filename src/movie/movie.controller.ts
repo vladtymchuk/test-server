9bf031e66624978fec57d578778eeac883553d9d
vladtymchuk <tymchukvlad177@gmail.com>
2025-08-07 14:22:37 +0300

    all generators

--- src/movie/movie.controller.ts
+++ src/movie/movie.controller.ts
@@ -1,11 +1,60 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Headers,
+  Param,
+  Post,
+  Query,
+  Req,
+  Res,
+} from '@nestjs/common';
 import { MovieService } from './movie.service';
+import { type Response, type Request } from 'express';
 
-@Controller('movie')
+@Controller('movies')
 export class MovieController {
   constructor(private readonly movieService: MovieService) {}
   @Get()
-  async test() {
-    return this.movieService.test();
+  findAll(@Query() query: any) {
+    return JSON.stringify(query);
+  }
+
+  @Get(':id/')
+  findById(@Param('id') id: string) {
+    return { id };
+  }
+
+  @Post()
+  create(@Body() body: { title: string; genre: string }) {
+    return body;
+  }
+
+  @Get('headers')
+  getHeaders(@Headers() headers: any) {
+    return headers;
+  }
+
+  @Get('user-agent')
+  getUserAgent(@Headers('user-agent') userAgent: string) {
+    return { userAgent };
+  }
+
+  @Get('request-details')
+  getRequestDetails(@Req() req: Request) {
+    return {
+      body: req.body,
+      headers: req.headers,
+      query: req.query,
+      params: req.params,
+      method: req.method,
+    };
+  }
+
+  @Get('response-details')
+  getResponseDetails(@Res() res: Response) {
+    res.status(201).json({
+      message: 'Success',
+    });
   }
 }
